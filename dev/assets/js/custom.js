4110cb00f387f3cfafcdc9b45ec7932089c81b30
Mackedonec <mackedonec187@gmail.com>
2023-07-03 22:38:26 +0300

    update

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -15,37 +15,72 @@ const countryInfo = [
   },
   {
     id: 3,
-    flag: "assets/img/India.png",
+    flag: "assets/img/United_States.png",
     name: "США",
     strength: 331,
     mainland: "north_america",
   },
   {
     id: 4,
-    flag: "assets/img/India.png",
+    flag: "assets/img/Indonesia.png",
     name: "Індонезія",
     strength: 273,
     mainland: "eurasia",
   },
   {
     id: 5,
-    flag: "assets/img/India.png",
+    flag: "assets/img/Pakistan.png",
     name: "Пакистан",
     strength: 220,
     mainland: "eurasia",
   },
   {
     id: 6,
-    flag: "assets/img/India.png",
+    flag: "assets/img/Brazil.png",
     name: "Бразилія",
     strength: 212,
     mainland: "south_america",
   },
   {
     id: 7,
-    flag: "assets/img/India.png",
+    flag: "assets/img/Nigeria.png",
     name: "Нігерія",
     strength: 206,
+    mainland: "africa",
+  },
+  {
+    id: 8,
+    flag: "assets/img/Bangladesh.png",
+    name: "Бангладеш",
+    strength: 164,
+    mainland: "eurasia",
+  },
+  {
+    id: 9,
+    flag: "assets/img/ussia.png",
+    name: "країна 404",
+    strength: 144,
+    mainland: "eurasia",
+  },
+  {
+    id: 10,
+    flag: "assets/img/Mexico.png",
+    name: "Мексика",
+    strength: 129,
+    mainland: "north_america",
+  },
+  {
+    id: 11,
+    flag: "assets/img/Japan.png",
+    name: "Японія",
+    strength: 126,
+    mainland: "eurasia",
+  },
+  {
+    id: 12,
+    flag: "assets/img/Ethiopia.png",
+    name: "Ефіопія",
+    strength: 114,
     mainland: "africa",
   },
 ];
@@ -82,7 +117,7 @@ window.addEventListener("load", function () {
           <div class="countries">
             <div class="country-flag"><img src="${country.flag}" class="img-fluid" alt="${country.name}"></div>
             <div class="country-name">${country.name}</div>
-            <div class="country-strength">${country.strength}</div>
+            <div class="country-strength">${country.strength} мільйонів</div>
             <div class="country-mainland">${mainlandName}</div>
           </div>
           `;
